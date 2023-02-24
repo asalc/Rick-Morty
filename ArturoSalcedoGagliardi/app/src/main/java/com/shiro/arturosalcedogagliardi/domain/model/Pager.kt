15ca6f10244a47aef494b16ebf2496438d34d66f
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 19:24:57 +0100

    Add Pager model

---
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/domain/model/Pager.kt
@@ -0,0 +1,8 @@
+package com.shiro.arturosalcedogagliardi.domain.model
+
+class Pager {
+    var count: Int? = null
+    var pages: Int? = null
+    var next: String? = null
+    var prev: String? = null
+}
