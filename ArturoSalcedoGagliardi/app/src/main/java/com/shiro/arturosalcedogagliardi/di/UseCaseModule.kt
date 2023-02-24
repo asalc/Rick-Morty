437fea2b59ecd865ffc59e7c982de00c85134c2c
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 22:59:42 +0100

    Add use case di module

---
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/di/UseCaseModule.kt
@@ -0,0 +1,41 @@
+package com.shiro.arturosalcedogagliardi.di
+
+import com.shiro.arturosalcedogagliardi.domain.repository.CharactersRepository
+import com.shiro.arturosalcedogagliardi.domain.use_cases.GetAllCharactersUseCase
+import com.shiro.arturosalcedogagliardi.domain.use_cases.GetCharacterDetailsUseCase
+import com.shiro.arturosalcedogagliardi.domain.use_cases.GetCharactersBySearchUseCase
+import com.shiro.arturosalcedogagliardi.domain.use_cases.impl.GetAllCharactersUseCaseImpl
+import com.shiro.arturosalcedogagliardi.domain.use_cases.impl.GetCharacterDetailsUseCaseImpl
+import com.shiro.arturosalcedogagliardi.domain.use_cases.impl.GetCharactersBySearchUseCaseImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun providesGetAllCharactersUseCase(
+        charactersRepository: CharactersRepository
+    ): GetAllCharactersUseCase =
+        GetAllCharactersUseCaseImpl(charactersRepository)
+
+    @Provides
+    @Singleton
+    fun providesGetCharacterDetailsUseCase(
+        charactersRepository: CharactersRepository
+    ): GetCharacterDetailsUseCase =
+        GetCharacterDetailsUseCaseImpl(charactersRepository)
+
+    @Provides
+    @Singleton
+    fun providesGetCharactersBySearchUseCase(
+        charactersRepository: CharactersRepository
+    ): GetCharactersBySearchUseCase =
+        GetCharactersBySearchUseCaseImpl(charactersRepository)
+
+}
