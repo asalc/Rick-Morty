53cd63ee2c42374c293239966326d05c01f9e282
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 19:01:00 +0100

    Add Constants file

---
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/helpers/Constants.kt
@@ -0,0 +1,9 @@
+package com.shiro.arturosalcedogagliardi.helpers
+
+object Constants {
+    //DIALOG CODES
+    const val DIALOG_TITLE = "dialog_title"
+    const val DIALOG_DESCRIPTION = "dialog_description"
+    const val DIALOG_ACCEPT = "dialog_accept"
+    const val DIALOG_CANCEL = "dialog_cancel"
+}
