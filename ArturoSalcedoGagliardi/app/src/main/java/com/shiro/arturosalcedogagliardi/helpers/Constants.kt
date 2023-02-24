307f04c8a227f471904d8f2f40a005d85ab2ed15
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 22:00:12 +0100

    Add HTTP Error codes constants

--- ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/helpers/Constants.kt
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/helpers/Constants.kt
@@ -1,6 +1,13 @@
 package com.shiro.arturosalcedogagliardi.helpers
 
 object Constants {
+
+    //WEB SERVICE CODES
+    const val BAD_REQUEST_CODE = 400
+    const val UNAUTHORIZED_CODE = 401
+    const val FORBIDDEN_CODE = 403
+    const val NOT_FOUND_CODE = 404
+
     //DIALOG CODES
     const val DIALOG_TITLE = "dialog_title"
     const val DIALOG_DESCRIPTION = "dialog_description"
