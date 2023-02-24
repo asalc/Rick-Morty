4657b9e10108e4fe435bef6d07ce9f6631ccd804
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 19:24:35 +0100

    Add CharactersApi interface

---
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/data/source/remote/api/CharactersApi.kt
@@ -0,0 +1,28 @@
+package com.shiro.arturosalcedogagliardi.data.source.remote.api
+
+import com.shiro.arturosalcedogagliardi.data.model.CharacterResultRemote
+import com.shiro.arturosalcedogagliardi.data.model.character.CharacterRemote
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CharactersApi {
+
+    @GET("character/")
+    suspend fun getCharacters(
+        @Query("page") page: Int
+    ): Response<CharacterResultRemote>
+
+    @GET("character/{characterId}")
+    suspend fun getCharacterDetails(
+        @Path("characterId") characterId: Int
+    ): Response<CharacterRemote>
+
+    @GET("character/")
+    suspend fun searchCharacters(
+        @Query("name") name: String,
+        @Query("page") page: Int
+    ): Response<CharacterResultRemote>
+
+}
