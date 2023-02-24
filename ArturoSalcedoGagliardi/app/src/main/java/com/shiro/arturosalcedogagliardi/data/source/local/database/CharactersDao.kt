8d8569b65781e6b192b4ada40afb600819d3fa2e
Arturo Salcedo <asalcedo2323@live.com>
2023-02-24 20:10:51 +0100

    Add CharactersDao

---
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/data/source/local/database/CharactersDao.kt
@@ -0,0 +1,22 @@
+package com.shiro.arturosalcedogagliardi.data.source.local.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.shiro.arturosalcedogagliardi.data.model.character.CharacterLocal
+
+@Dao
+interface CharactersDao {
+    @Query("SELECT * FROM CharacterLocal")
+    fun getAllCharacters(): List<CharacterLocal>
+
+    @Query("SELECT * FROM CharacterLocal WHERE id = :characterId")
+    fun getCharacterDetails(characterId: Int): CharacterLocal
+
+    @Query("SELECT * FROM CharacterLocal WHERE name LIKE :name")
+    fun searchCharacters(name: String): List<CharacterLocal>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCharacter(character: CharacterLocal)
+}
