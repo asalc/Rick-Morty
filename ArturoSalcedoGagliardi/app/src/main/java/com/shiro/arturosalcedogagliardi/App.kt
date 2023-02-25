99b780c1e25313ad608e9c542443058db9b0b9a4
Arturo Salcedo <asalcedo2323@live.com>
2023-02-25 09:07:08 +0100

    Add instance property to App class

--- ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/App.kt
+++ ArturoSalcedoGagliardi/app/src/main/java/com/shiro/arturosalcedogagliardi/App.kt
@@ -4,4 +4,14 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+    }
+}
